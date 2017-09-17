3ee37498d1db10769ce5203801911010c0dc024a
Dmitriy Safronov <zimniy@cyberbrain.pw>
2017-09-17 16:56:31 +0300

    Fix env

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ export ACCOUNT_MAINTENANCE='ssh-rsa YourAnsibleSshPublicKey Maintenance account'
 
 if [[ -n "$(hostname -d)" && -n "${ROOTMAIL}" && -n "${ACCOUNT_EMERGENCY}" && -n "${ACCOUNT_MAINTENANCE}" ]]; then
 	wget https://github.com/DmitriySafronov/ubuntu-vm-config/raw/master/install.sh -O ~/ubuntu-vm-config-install.sh
-	. ~/ubuntu-vm-config-install.sh
+	bash ~/ubuntu-vm-config-install.sh
 	rm -f ~/ubuntu-vm-config-install.sh 2> /dev/null
 	rm -f ~/.wget-hsts 2> /dev/null
 	cat /dev/null > ~/.bash_history && history -c
--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ -n "$(hostname -d)" ]]; then
-	clear; echo "Set up FQDN first!"
+if [[ -n "$(hostname -d)" && -n "${ROOTMAIL}" && -n "${ACCOUNT_EMERGENCY}" && -n "${ACCOUNT_MAINTENANCE}" ]]; then
+	clear; echo "Set up prerequesites first!"
 	exit 1
 else
 	wget https://github.com/DmitriySafronov/ubuntu-vm-config/raw/master/bootstrap/accounts.sh -O /tmp/ubuntu-vm-config.sh
-	. /tmp/ubuntu-vm-config.sh && rm -f /tmp/ubuntu-vm-config.sh 2> /dev/null
+	call /tmp/ubuntu-vm-config.sh && rm -f /tmp/ubuntu-vm-config.sh 2> /dev/null
 	
 	wget https://github.com/DmitriySafronov/ubuntu-vm-config/raw/master/bootstrap/software.sh -O /tmp/ubuntu-vm-config.sh
-	. /tmp/ubuntu-vm-config.sh && rm -f /tmp/ubuntu-vm-config.sh 2> /dev/null
+	call /tmp/ubuntu-vm-config.sh && rm -f /tmp/ubuntu-vm-config.sh 2> /dev/null
 fi
