520dce207836472aba4409d03dc2274e70db9d0f
Dmitriy Safronov <zimniy@cyberbrain.pw>
2017-09-17 18:04:51 +0300

    test-05

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-A_BOOTSTRAPS=accounts.sh software.sh
-
 if [[ -z "$(hostname -d)" || -z "${ROOTMAIL}" || -z "${ACCOUNT_EMERGENCY}" || -z "${ACCOUNT_MAINTENANCE}" ]]; then
 	clear; echo "Set up prerequesites first!"
 else
-	for A_BOOTSTRAP in ${A_BOOTSTRAPS}; do
-		wget https://github.com/DmitriySafronov/ubuntu-vm-config/raw/master/bootstrap/${A_BOOTSTRAP} -O /tmp/ubuntu-vm-config.sh
-		source /tmp/ubuntu-vm-config.sh && rm -f /tmp/ubuntu-vm-config.sh 2> /dev/null
-	done
+	wget https://github.com/DmitriySafronov/ubuntu-vm-config/raw/master/bootstrap/accounts.sh -O /tmp/ubuntu-vm-config.sh
+	source /tmp/ubuntu-vm-config.sh && rm -f /tmp/ubuntu-vm-config.sh 2> /dev/null
+	
+	wget https://github.com/DmitriySafronov/ubuntu-vm-config/raw/master/bootstrap/software.sh -O /tmp/ubuntu-vm-config.sh
+	source /tmp/ubuntu-vm-config.sh && rm -f /tmp/ubuntu-vm-config.sh 2> /dev/null
 fi
