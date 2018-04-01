7eab5d3d50898fd2017d168ee5bc5d23b76c243b
Dmitriy Safronov <zimniy@cyberbrain.pw>
2018-04-01 16:29:52 +0300

    Check for correct setup 1

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 if [[ -z "${ROOTMAIL}" || -z "${ACCOUNT_EMERGENCY}" || -z "${ACCOUNT_MAINTENANCE}" ]]; then
 	clear; echo "Set up prerequesites first!"
+	break
 else
 	wget https://github.com/DmitriySafronov/ubuntu-vm-config/raw/master/bootstrap/accounts.sh -O /tmp/ubuntu-vm-config.sh
 	source /tmp/ubuntu-vm-config.sh && rm -f /tmp/ubuntu-vm-config.sh 2> /dev/null
