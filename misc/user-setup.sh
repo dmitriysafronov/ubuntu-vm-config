12542fbddc2c564681ba39c19e333e6502ff2e5d
Dmitriy Safronov <zimniy@cyberbrain.pw>
2018-04-01 18:52:04 +0300

    Tmux on login for non-system users only

--- misc/user-setup.sh
+++ misc/user-setup.sh
@@ -4,8 +4,11 @@
 apt install -y --no-install-recommends bash-completion tmux mc unzip htop most command-not-found configure-debian psmisc man-db ncdu
 
 # Step: tmux on login
-echo -e "if [[ -n \"\$(which tmux)\" ]]; then
-\tif [[ -z \"\$TMUX\" ]]; then
-\t\ttmux has-session -t login || exec tmux new-session -s login && exec tmux attach-session -d -t login
+echo -e "if [[ $(id -u) -eq 0 || $(id -u) -ge 1000 ]]; then
+\tif [[ -n \"$(which tmux)\" ]]; then
+\t\tif [[ -z \"$TMUX\" ]]; then
+\t\t\ttmux has-session -t login || exec tmux new-session -s login && exec tmux attach-session -d -t login
+\t\tfi
 \tfi
-fi" > /etc/profile.d/tmux.sh
+fi
+" > /etc/profile.d/tmux.sh
