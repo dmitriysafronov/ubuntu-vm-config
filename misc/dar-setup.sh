33412cde1c1174f1d894d56801ddebb1eb2533f4
Dmitriy Safronov <DmitriySafronov@users.noreply.github.com>
2018-04-05 19:55:36 +0300

    line wrap 2

--- misc/dar-setup.sh
+++ misc/dar-setup.sh
@@ -6,3 +6,5 @@ chown root:root /usr/local/sbin/reconfigure-domain-access
 chmod 0750 /usr/local/sbin/reconfigure-domain-access
 /usr/local/sbin/reconfigure-domain-access
 head -n -1 /etc/rc.local > /tmp/rc.local.tmp; grep -q '/usr/local/sbin/reconfigure-domain-access' /tmp/rc.local.tmp || `echo -e "/usr/local/sbin/reconfigure-domain-access\n\nexit 0" >> /tmp/rc.local.tmp; cat /tmp/rc.local.tmp > /etc/rc.local`
+
+#
