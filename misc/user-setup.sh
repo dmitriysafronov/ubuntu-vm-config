6cdb049165ee7d3ee34ab4d4ddc24e6c4166ac51
Dmitriy Safronov <DmitriySafronov@users.noreply.github.com>
2018-04-05 19:52:00 +0300

    Escaping characters

--- misc/user-setup.sh
+++ misc/user-setup.sh
@@ -4,9 +4,9 @@
 apt install -y --no-install-recommends bash-completion tmux mc unzip htop most command-not-found configure-debian psmisc man-db ncdu
 
 # Step: tmux on login
-echo -e "if [[ $(id -u) -eq 0 || $(id -u) -ge 1000 ]]; then
-\tif [[ -n \"$(which tmux)\" ]]; then
-\t\tif [[ -z \"$TMUX\" ]]; then
+echo -e "if [[ \${UID} -eq 0 || \${UID} -ge 1000 ]]; then
+\tif [[ -n \"\$(which tmux)\" ]]; then
+\t\tif [[ -z \"\${TMUX}\" ]]; then
 \t\t\ttmux has-session -t login || exec tmux new-session -s login && exec tmux attach-session -d -t login
 \t\tfi
 \tfi
