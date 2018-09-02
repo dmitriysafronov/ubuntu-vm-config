da9adc866a81c34c169dfa266e47e4a6a522a3eb
Dmitriy Safronov <DmitriySafronov@users.noreply.github.com>
2018-09-02 15:21:28 +0300

    rc.local

--- bootstrap/software.sh
+++ bootstrap/software.sh
@@ -87,6 +87,26 @@ update-grub
 
 ###############################################################
 
+if [[ ! -x /etc/rc.local ]]; then
+echo -e "#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will \"exit 0\" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+exit 0 > /etc/rc.local
+	chmod ug+x /etc/rc.local
+fi
+
+###############################################################
+
 # Step: postfix reconfigurator
 wget https://github.com/DmitriySafronov/ubuntu-vm-config/raw/master/sbin/reconfigure-postfix -O /usr/local/sbin/reconfigure-postfix
 chown root:root /usr/local/sbin/reconfigure-postfix
